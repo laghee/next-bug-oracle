d79fb55c775d21338527f9baaaea6acc3ff9fee3
laghee <katrua@gmail.com>
2018-06-04 15:30:17 +0200

    No issue. Add bug model and pipenv config file.

--- next_bug_oracle/nbo/models.py
+++ next_bug_oracle/nbo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Bug(models.Model):
+    bug_num = models.IntegerField
+    bug_text = models.TextField
+    bug_date = models.DateTimeField
+    bug_source = models.CharField(max_length=8)
+    bug_link = models.URLField
+
+    def __str__(self):
+        return self.bug_num + ': ' + self.bug_text
