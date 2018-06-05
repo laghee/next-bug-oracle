306b58b3fa3280aea4bf8710797356253393c58f
laghee <katrua@gmail.com>
2018-06-05 20:19:38 +0200

    No issue. Clean up comments in urls and revise bug properties.

--- next_bug_oracle/nbo/models.py
+++ next_bug_oracle/nbo/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Bug(models.Model):
-    bug_num = models.IntegerField
+    bug_id = models.IntegerField(primary_key=True)
     bug_text = models.TextField
-    bug_date = models.DateTimeField
-    bug_source = models.CharField(max_length=8)
-    bug_link = models.URLField
+    report_date = models.DateTimeField
+    source = models.CharField(max_length=8)
+    link = models.URLField
 
     def __str__(self):
-        return self.bug_num + ': ' + self.bug_text
+        return self.bug_id + ': ' + self.bug_text
