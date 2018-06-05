6dc41fc0afd2767f62eb44d7ae8ba560c2b5c7f0
laghee <katrua@gmail.com>
2018-06-05 20:21:03 +0200

    No issue. Get basic api functionality working in super ugly way.

--- next_bug_oracle/nbo/views.py
+++ next_bug_oracle/nbo/views.py
@@ -3,72 +3,70 @@ from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
-from django.utils import timezone
+import requests
+import json
 
-# from .models import Choice, Question
+from .models import Bug
 
 
 def index(request):
-    # template = loader.get_template('nbo/index.html')
     return render(request, 'nbo/index.html')
 
 
-# class IndexView(generic.ListView):
-#     template_name = 'polls/index.html'
-#     context_object_name = 'latest_question_list'
+def result(request):
+    gh_url = 'https://api.github.com/search/issues?q='
+    bz_url = 'https://bugzilla.mozilla.org/rest/bug'
 
-#     def get_queryset(self):
-#         """Return the last five published questions (not including
-#         those set to be published in the future).
-#         """
-#         return Question.objects.filter(
-#             pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+    gh1 = 'repo:webcompat/web-bugs+state:open+label:priority-critical+-author:webcompat-bot&sort=created&order=asc'
+    bz1_query = {
+        'include_fields': 'id,summary,creation_time', 'product':
+        'tech evangelism', 'component': ['desktop', 'mobile'], 'priority':
+        'p1', 'whiteboard': 'needsdiagnosis', 'f1': 'resolution', 'o1':
+        'isempty', 'order': 'creation_time%20asc'}
+    gh2a = 'repo:webcompat/web-bugs+state:open+label:priority-important+-author:webcompat-bot&sort=created&order=asc'
+    gh2b = 'repo:webcompat/web-bugs+state:open+label:priority-critical+author:webcompat-bot&sort=created&order=asc'
+    bz3_query = {
+        'include_fields': 'id,summary,creation_time', 'product':
+            'tech evangelism', 'component': ['desktop', 'mobile'], 'priority':
+            'p1', 'whiteboard': 'needsdiagnosis', 'f1': 'resolution', 'o1':
+            'isempty', 'order': 'creation_time%20asc'}
+    gh3 = 'repo:webcompat/web-bugs+state:open+label:priority-normal&sort=created&order=asc'
 
+    bug = Bug()
 
-# class DetailView(generic.DetailView):
-#     model = Question
-#     template_name = 'polls/detail.html'
+    r = requests.get(bz_url, params=bz1_query)
 
-#     def get_queryset(self):
-#         """
-#         Excludes any questions that aren't published yet.
-#         """
-#         return Question.objects.filter(pub_date__lte=timezone.now())
+    if r.status_code == requests.codes.ok:
+        try:
+            bug_list = r.json()
+            if len(bug_list['bugs']) > 0:
+                bug.bug_id = bug_list['bugs'][0]['id']
+                bug.bug_text = bug_list['bugs'][0]['summary']
+                bug.report_date = bug_list['bugs'][0]['creation_time']
+                bug.source = 'bugzilla'
+                bug_url_string = 'https://bugzilla.mozilla.org/show_bug.cgi?id=' + \
+                    str(bug.bug_id)
+                bug.link = bug_url_string
+            else:
+                r = requests.get(gh_url + gh1)
 
+                if r.status_code == requests.codes.ok:
+                    try:
+                        bug_list = r.json()
+                        if bug_list['total_count'] > 0:
+                            bug.bug_id = bug_list['items'][0]['number']
+                            bug.bug_text = bug_list['items'][0]['title']
+                            bug.report_date = bug_list['items'][0]['created_at']
+                            bug.source = 'github'
+                            bug.link = bug_list['items'][0]['html_url']
+                        else:
+                            print("No bugs for you -- something wrong with GH call.")
+                    except ValueError:
+                        print("There's a problem with your json data.")
+        except ValueError:
+            print("There's a problem with your json data.")
+    else:
+        print("HTTP response code: " + r.status_code)
 
-# class ResultsView(generic.DetailView):
-#     model = Question
-#     template_name = 'polls/results.html'
-
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     context = {'latest_question_list': latest_question_list}
-#     return render(request, 'polls/index.html', context)
-
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/detail.html', {'question': question})
-
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/results.html', {'question': question})
-
-
-# def vote(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     try:
-#         selected_choice = question.choice_set.get(pk=request.POST['choice'])
-#     except (KeyError, Choice.DoesNotExist):
-#         # Redisplay the question voting form.
-#         return render(request, 'polls/detail.html', {
-#             'question': question,
-#             'error_message': "You didn't select a choice.",
-#         })
-#     else:
-#         selected_choice.votes += 1
-#         selected_choice.save()
-#         # Always return an HttpResponseRedirect after successfully dealing
-#         # with POST data. This prevents data from being posted twice if a
-#         # user hits the Back button.
-#         return HttpResponseRedirect(reverse('polls:results',
-#                                             args=(question_id,)))
+    context = {'bug': bug}
+    return render(request, 'nbo/result.html', context)
